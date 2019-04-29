1f1dfc6afe43fc31399458c903e1d079f490bf2f
Wittavat Kanyarong <nickson_kv@hotmail.com>
2019-04-29 15:33:33 +0700

    add style

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,11 +6,43 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showAppName() {
+    return Text(
+      "My นิคนะ Name",
+      style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.blue,
+          fontFamily: 'Charm'),
+    );
+  }
+
+  Widget showLogo() {
+    return Image.asset('images/logo.png');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(alignment: Alignment(0, 0),
-        child: Text('This is body'),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.white, Colors.green],begin: Alignment(0, -1))),
+        padding: EdgeInsets.only(top: 100.0),
+        alignment: Alignment(0, -1),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 100.0,
+              height: 100.0,
+              child: showLogo(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 15.0),
+              child: showAppName(),
+            )
+          ],
+        ),
       ),
     );
   }
