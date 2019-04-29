0fe5bb9c531a9b340ea586a0676c869f62d9d77d
Wittavat Kanyarong <nickson_kv@hotmail.com>
2019-04-29 16:36:34 +0700

    done 29 april

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,6 +6,41 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget signUpButton() {
+    return RaisedButton(
+      color: Colors.orange[600],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget signInButton() {
+    return RaisedButton(
+      color: Colors.orange,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      child: Text('Sign In'),
+      onPressed: () {},
+    );
+  }
+
+  Widget passwordTextFormField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: '8 bit',
+      ),
+      obscureText: true,
+    );
+  }
+
+  Widget emailTextFromField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'Email :', hintText: 'Your@email.com'),
+    );
+  }
+
   Widget showAppName() {
     return Text(
       "My นิคนะ Name",
@@ -24,10 +59,11 @@ class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
-                colors: [Colors.white, Colors.green],begin: Alignment(0, -1))),
+                colors: [Colors.white, Colors.green], begin: Alignment(0, -1))),
         padding: EdgeInsets.only(top: 100.0),
         alignment: Alignment(0, -1),
         child: Column(
@@ -40,6 +76,33 @@ class _AuthenState extends State<Authen> {
             Container(
               margin: EdgeInsets.only(top: 15.0),
               child: showAppName(),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 20.0),
+              child: emailTextFromField(),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 50.0, right: 50.0),
+              child: passwordTextFormField(),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 20.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(right: 4.0),
+                      child: signInButton(),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(right: 4.0),
+                      child: signUpButton(),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
